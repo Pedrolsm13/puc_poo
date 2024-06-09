4a8fd68a0195477ce88ed03c9309679d3ea66385
Pedro Lucas <plserramoura@gmail.com>
2024-06-09 13:25:39 -0300

    atv

---
+++ ProjetoTesteArquivo/ProjetoTesteArquivo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+
+namespace ProjetoTesteArquivo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StreamReader rd = new StreamReader(@"C:\ATV_POO\customers-100.csv");
+            int nlinha = 0;
+            while (!rd.EndOfStream)
+            {
+                string linha = rd.ReadLine();
+                Console.WriteLine(linha);
+                nlinha++;
+                if(nlinha % 20 == 0)
+                {
+                    Console.WriteLine("cliqe para continuar");
+                    Console.ReadKey();
+                }
+            }
+            rd.Close();
+            Console.Write("\n\n\nPressione qualquer tecla para sair...");
+            Console.ReadKey(true);
+        }
+    }
+}
